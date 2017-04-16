066a9f7bf4a8f8a3c0c31126c298e7f2fb2f1cc0
Ben Gamari <ben@smart-cactus.org>
2017-04-16 16:38:10 -0400

    Show message during pass

--- ghc-dump-core/GhcDump/Plugin.hs
+++ ghc-dump-core/GhcDump/Plugin.hs
@@ -7,6 +7,7 @@ import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Binary.Serialise.CBOR as CBOR
 import GhcPlugins hiding (TB)
 import DynFlags (getDynFlags, dumpPrefix)
+import ErrUtils (showPass)
 import CoreMonad (CoreToDo(CoreDoPluginPass))
 
 import GhcDump.Convert
@@ -17,18 +18,20 @@ plugin = defaultPlugin { installCoreToDos = install }
 install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
 install opts todo = do
     dflags <- getDynFlags
-    let prefix = fromMaybe "dump" $ dumpPrefix dflags
-    return (intersperseDumps prefix todo)
+    return (intersperseDumps dflags todo)
 
-intersperseDumps :: String -> [CoreToDo] -> [CoreToDo]
-intersperseDumps dumpPrefix = go 0
+intersperseDumps :: DynFlags -> [CoreToDo] -> [CoreToDo]
+intersperseDumps dflags = go 0
   where
     go n (todo : rest) = pass n : todo : go (n+1) rest
     go n [] = [pass n]
 
-    pass n = CoreDoPluginPass "DumpCore" (liftIO . dumpIn dumpPrefix n)
+    pass n = CoreDoPluginPass "DumpCore" (liftIO . dumpIn dflags n)
 
-dumpIn :: String -> Int -> ModGuts -> IO ModGuts
-dumpIn dumpPrefix n guts = do
-    BSL.writeFile (dumpPrefix++"pass-"++show n++".cbor") $ CBOR.serialise (cvtModule guts)
+dumpIn :: DynFlags -> Int -> ModGuts -> IO ModGuts
+dumpIn dflags n guts = do
+    let prefix = fromMaybe "dump" $ dumpPrefix dflags
+        fname = prefix++"pass-"++show n++".cbor"
+    showPass dflags $ "GhcDump: Dumping core to "++fname
+    BSL.writeFile fname $ CBOR.serialise (cvtModule guts)
     return guts
