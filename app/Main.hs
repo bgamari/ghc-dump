120c7d9535816e090074ad33a6b549ba5e5caa21
Paul Brinkmeier <paul.brinkmeier@gmail.com>
2020-05-18 19:19:55 +0200

    Add context and other datastructures from AEATSD

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,49 @@
 module Main where
 
 import Codec.Serialise (deserialise)
+import Control.Monad (mapM_)
+import Data.List (find)
 import qualified Data.ByteString.Lazy as BSL
-import GhcDump.Ast (SModule)
+import qualified Data.Text as T
+import GhcDump.Ast
 import System.Environment (getArgs)
 
-import CoreDiff.Diff (someFunc)
+import CoreDiff.Diff (diffCore)
 
 main :: IO ()
 main = main' =<< getArgs
 
--- dump single file
-main' [path] = do
-  mod <- readModFile path
-  print mod
+-- diff files on a specific binding
+main' [binding, pathA, pathB] = do
+  modA <- readModFile pathA
+  modB <- readModFile pathB
 
--- diff files
-{-
-main' [pathA, pathB] = do
-  ...
--}
+  let Just (_, _, exprA) = lookupBinding binding modA
+  let Just (_, _, exprB) = lookupBinding binding modB
+
+  print exprA
+  print exprB
+
+  -- let patch = diffCore exprA exprB
+
+  -- print patch
 
 main' _      = putStrLn "Incorrect number of arguments, aborting."
 
 readModFile :: FilePath -> IO SModule
 readModFile path = deserialise <$> BSL.readFile path
+
+printMod :: SModule -> IO ()
+printMod mod = do
+  putStrLn $ "module " ++ (T.unpack $ showModName $ moduleName mod) ++ "[" ++ (T.unpack $ modulePhase mod) ++  "] where"
+  mapM_ print $ moduleTopBindings mod
+
+  where
+    showModName = getModuleName
+
+lookupBinding :: String -> SModule -> Maybe (SBinder, CoreStats, SExpr)
+lookupBinding binding mod = find go $ moduleBindings mod
+  where go (binder, _, _) = binding == getName binder
+
+getName :: SBinder -> String
+getName = T.unpack . binderName . unSBndr
--- lib/CoreDiff/Diff.hs
+++ lib/CoreDiff/Diff.hs
@@ -1,4 +1,40 @@
-module CoreDiff.Diff (someFunc) where
+module CoreDiff.Diff where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import GhcDump.Ast
+
+type MetaVar = Int
+type CorePatch = CoreChange (CoreChange MetaVar)
+
+type CoreChange var = (CoreC var, CoreC var)
+
+data CoreC metavar
+  = EVarC BinderId
+  | EVarGlobalC ExternalName
+  | ELitC Lit
+  | EAppC (CoreC metavar) (CoreC metavar)
+  | ETyLamC SBinder (CoreC metavar)
+  | ELamC SBinder (CoreC metavar)
+  | ELetC [(SBinder, CoreC metavar)] (CoreC metavar)
+  | ECaseC (CoreC metavar) SBinder [AltC metavar]
+  | EType (TypeC metavar)
+  | ECoercionC
+  | CoreHole metavar
+
+data AltC metavar = AltC
+  { altCon :: AltCon
+  , altBinders :: [SBinder]
+  , altRHS :: CoreC metavar
+  }
+
+data TypeC metavar
+  = VarTyC BinderId
+  | FunTyC (TypeC metavar)
+  | TyConAppC TyCon [TypeC metavar]
+  | AppTyC (TypeC metavar) (TypeC metavar)
+  | ForAllTyC SBinder (TypeC metavar)
+  | LitTyC
+  | CoercionTyC
+  | TypeHole metavar
+
+diffCore :: SExpr -> SExpr -> CorePatch
+diffCore = error "unimplemented"
