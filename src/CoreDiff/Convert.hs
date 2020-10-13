250125ad7bbd9ae4bdf051f72e7c4a620313b260
Paul Brinkmeier <paul.brinkmeier@gmail.com>
2020-10-13 15:20:07 +0200

    Implement conversion and pretty-printing for binder metadata

--- src/CoreDiff/Convert.hs
+++ src/CoreDiff/Convert.hs
@@ -46,7 +46,32 @@ cvtMeta (Bndr binder@Binder{}) = XBinderMeta
     idi = binderIdInfo binder
 
 cvtType :: Type -> XType UD
-cvtType = error "stub"
+cvtType (VarTy binder) = XVarTy $ cvtBinder binder
+cvtType (FunTy l r) = XFunTy (cvtType l) (cvtType r)
+cvtType (TyConApp tc args) = XTyConApp (cvtTyCon tc) (map cvtType args)
+  where cvtTyCon (TyCon name _) = TyCon' name
+cvtType (AppTy l r) = XAppTy (cvtType l) (cvtType r)
+cvtType (ForAllTy binder ty) = XForAllTy (cvtBinder binder) (cvtType ty)
+cvtType (LitTy) = XLitTy
+cvtType (CoercionTy) = XCoercionTy
 
 cvtExpr :: Expr -> XExpr UD
-cvtExpr = error "stub"
+cvtExpr (EVar binder) = XVar $ cvtBinder binder
+cvtExpr (EVarGlobal extName) = XVarGlobal $ cvtExtName extName
+  where cvtExtName (ExternalName modName name _) = ExternalName' (getModuleName modName) name
+cvtExpr (ELit lit) = XLit lit
+cvtExpr (EApp f x) = XApp (cvtExpr f) (cvtExpr x)
+cvtExpr (ETyLam binder expr) = XTyLam (cvtBinder binder) (cvtExpr expr)
+cvtExpr (ELam binder expr) = XLam (cvtBinder binder) (cvtExpr expr)
+cvtExpr (ELet bindings body) = XLet
+  (map cvtBinding bindings)
+  (cvtExpr body)
+cvtExpr (ECase match binder alts) = XCase
+  (cvtExpr match)
+  (cvtBinder binder)
+  (map cvtAlt alts)
+cvtExpr (EType ty) = XType $ cvtType ty
+cvtExpr (ECoercion) = XCoercion
+
+cvtAlt :: Alt -> XAlt UD
+cvtAlt (Alt con binders rhs) = XAlt con (map cvtBinder binders) (cvtExpr rhs)
--- src/CoreDiff/PrettyPrint.hs
+++ src/CoreDiff/PrettyPrint.hs
@@ -6,6 +6,7 @@ module CoreDiff.PrettyPrint where
 import Control.Monad.Trans.Reader
 import Data.Maybe
 import qualified Data.Text as T
+import GhcDump.Ast (IdScope(..), OccInfo(..))
 import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
 
 
@@ -41,6 +42,7 @@ instance PprWithOpts XModule where
 
 
 instance PprWithOpts (XBinding a) where
+  pprWithOpts (XXBinding _) = error "stub"
   pprWithOpts binding = do
     opts <- ask
     if pprOptsLongBindings opts then
@@ -59,7 +61,7 @@ instance PprWithOpts (XBinding a) where
       pprSignature :: XBinder a -> Reader PprOpts Doc
       pprSignature binder = do
         opts <- ask
-        metaDoc <- pprWithOpts $ xBinderMeta binder
+        metaDoc <- pprBinderMeta $ xBinderMeta binder
         nameDoc <- pprBinderName binder
         typeDoc <- pprWithOpts $ xBinderTypeOrKind binder
 
@@ -72,22 +74,54 @@ instance PprWithOpts (XBinding a) where
       pprShortBinding (XBinding binder expr) = do
         opts <- ask
         binderNameDoc <- pprBinderName binder
-        metaDoc <- pprWithOpts $ xBinderMeta binder
+        metaDoc <- pprBinderMeta $ xBinderMeta binder
         typeDoc <- pprWithOpts $ xBinderTypeOrKind binder
         exprDoc <- pprWithOpts expr
 
         return $ hang' (hsep $ catMaybes [ Just binderNameDoc, toMaybe (pprOptsDisplayMeta opts) metaDoc, Just dcolon, Just typeDoc, Just equals]) 2 exprDoc
 
 
+instance PprWithOpts (XBinder a) where
+  pprWithOpts (XXBinder _) = error "stub"
+  pprWithOpts binder = pprBinderName binder
+
+
 instance PprWithOpts (XType a) where
-  pprWithOpts _ = return $ text "tbd"
+  pprWithOpts (XXType _) = error "stub"
+  pprWithOpts ty = pprType TopPrec ty
+  
+data TyPrec = TopPrec | FunPrec | TyOpPrec | TyConPrec
+  deriving (Eq, Ord)
+
+pprType :: TyPrec -> XType a -> Reader PprOpts Doc
+pprType _    (XVarTy binder) = pprBinderName binder
+pprType prec t@XFunTy{} = do
+  funTyDocs <- mapM (pprType FunPrec) funTys
+  return $ parensIf (prec >= FunPrec) $ sep $ punctuate " ->" funTyDocs
+    where funTys = collectFunTys t
+-- TODO: special tycons like [], (,), etc.
+pprType _    (XTyConApp tyCon []) = return $ pprTyCon tyCon
+pprType prec (XTyConApp tyCon tys) = do
+  tyDocs <- mapM (pprType TyConPrec) tys
+  return $ parensIf (prec >= FunPrec) $ pprTyCon tyCon <+> hsep tyDocs
+pprType prec (XAppTy f x) = do
+  fDoc <- pprType TyConPrec f
+  xDoc <- pprType TyConPrec x
+  return $ parensIf (prec >= TyConPrec) $ fDoc <+> xDoc
+pprType prec t@XForAllTy{} = do
+  binderDocs <- mapM pprBinderName binders
+  tyDoc <- pprType TopPrec ty
+  return $ parensIf (prec >= TyOpPrec) $ "forall" <+> hsep binderDocs <> dot <+> tyDoc
+  where
+  (binders, ty) = collectForAlls t
+
 
 instance PprWithOpts (XExpr a) where
+  pprWithOpts (XXExpr _) = error "stub"
   pprWithOpts _ = return $ text "tbd"
 
-instance PprWithOpts XBinderMeta where
-  pprWithOpts _ = return $ text "tbd"
-
+instance PprWithOpts (XAlt a) where
+  pprWithOpts (XXAlt _) = error "stub"
 
 -- Terminals
 
@@ -99,6 +133,34 @@ pprBinderName binder = do
   else
     return $ text' $ xBinderName binder
 
+pprBinderMeta :: XBinderMeta -> Reader PprOpts Doc
+pprBinderMeta meta = do
+  occInfoDoc <- pprOccInfo $ xbmOccInfo meta
+
+  return $ brackets $ align $ sep $ punctuate "," $ catMaybes $
+    [ Just $ pprScope $ xbmScope meta
+    , toMaybe (xbmArity meta /= 0)               $ "arity=" <> pretty (xbmArity meta)
+    , toMaybe (xbmInlinePragma meta /= "")       $ "inline=" <> text' (xbmInlinePragma meta)
+    , Just $ "occ=" <> occInfoDoc
+    , toMaybe (xbmStrictnessSig meta /= "")      $ "str=" <> text' (xbmStrictnessSig meta)
+    , toMaybe (xbmDemandSig meta /= "")          $ "dmd=" <> text' (xbmDemandSig meta)
+    , toMaybe (xbmCpr meta /= "")                $ "cpr=" <> text' (xbmCpr meta)
+    , toMaybe (xbmCallArity meta /= 0)           $ "call-arity=" <> pretty (xbmCallArity meta)
+    , toMaybe (xbmIsOneShot meta)                $ "one-shot"
+    ]
+  where
+    pprScope GlobalId = "GblId"
+    pprScope LocalIdX = "LclIdX"
+    pprScope LocalId  = "LclId"
+
+    pprOccInfo OccManyOccs = return "Many"
+    pprOccInfo OccDead = return "Dead"
+    pprOccInfo OccOneOcc = return "One"
+    pprOccInfo (OccLoopBreaker isStrong) =
+      return $ (if isStrong then "Strong" else "Weak") <+> "Loopbrk"
+
+pprTyCon (TyCon' t) = text' t
+
 -- Helper functions
 
 dcolon = text "::"
@@ -108,3 +170,16 @@ hang' left indent right = hang indent $ sep [ left, right ]
 
 toMaybe True  x = Just x
 toMaybe False _ = Nothing
+
+parensIf True  = parens
+parensIf False = id
+
+collectFunTys :: XType a -> [XType a]
+collectFunTys = go []
+  where go acc (XFunTy l r) = go (l : acc) r
+        go acc ty           = reverse $ ty : acc
+
+collectForAlls :: XType a -> ([XBinder a], XType a)
+collectForAlls = go []
+  where go acc (XForAllTy binder ty) = go (binder : acc) ty
+        go acc ty                    = (reverse acc, ty)
