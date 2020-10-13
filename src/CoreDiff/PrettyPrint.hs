bf8ac26191dd9df84c0576fa71bc4e64f037dd09
Paul Brinkmeier <paul.brinkmeier@gmail.com>
2020-10-13 16:00:02 +0200

    Implement pretty-printing

--- src/CoreDiff/PrettyPrint.hs
+++ src/CoreDiff/PrettyPrint.hs
@@ -4,9 +4,11 @@
 module CoreDiff.PrettyPrint where
 
 import Control.Monad.Trans.Reader
+import qualified Data.ByteString.Char8 as BS
 import Data.Maybe
+import Data.Ratio
 import qualified Data.Text as T
-import GhcDump.Ast (IdScope(..), OccInfo(..))
+import GhcDump.Ast (IdScope(..), OccInfo(..), Lit(..), AltCon(..))
 import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
 
 
@@ -42,6 +44,8 @@ instance PprWithOpts XModule where
 
 
 instance PprWithOpts (XBinding a) where
+  -- big rule of thumb: each global function that uses ppr-functions other that pprWithOpts, which is guaranteed to handle extensions, must handle extensions itself.
+  -- Also true for any functions that pattern-match. basically all PprWithOpts instances and then some.
   pprWithOpts (XXBinding _) = error "stub"
   pprWithOpts binding = do
     opts <- ask
@@ -51,9 +55,10 @@ instance PprWithOpts (XBinding a) where
       pprShortBinding binding
     where
       pprLongBinding :: XBinding a -> Reader PprOpts Doc
+      pprLongBinding (XXBinding _) = error "stub"
       pprLongBinding (XBinding binder expr) = do
         sigDoc <- pprSignature binder
-        binderNameDoc <- pprBinderName binder
+        binderNameDoc <- pprWithOpts binder
         exprDoc <- pprWithOpts expr
 
         return $ vsep [ sigDoc, hang' (binderNameDoc <+> equals) 2 exprDoc ]
@@ -62,7 +67,7 @@ instance PprWithOpts (XBinding a) where
       pprSignature binder = do
         opts <- ask
         metaDoc <- pprBinderMeta $ xBinderMeta binder
-        nameDoc <- pprBinderName binder
+        nameDoc <- pprWithOpts binder
         typeDoc <- pprWithOpts $ xBinderTypeOrKind binder
 
         return $ vsep $ catMaybes
@@ -72,29 +77,33 @@ instance PprWithOpts (XBinding a) where
 
       pprShortBinding :: XBinding a -> Reader PprOpts Doc
       pprShortBinding (XBinding binder expr) = do
-        opts <- ask
-        binderNameDoc <- pprBinderName binder
-        metaDoc <- pprBinderMeta $ xBinderMeta binder
-        typeDoc <- pprWithOpts $ xBinderTypeOrKind binder
+        binderDoc <- pprWithOpts binder
         exprDoc <- pprWithOpts expr
 
-        return $ hang' (hsep $ catMaybes [ Just binderNameDoc, toMaybe (pprOptsDisplayMeta opts) metaDoc, Just dcolon, Just typeDoc, Just equals]) 2 exprDoc
+        return $ hang' (binderDoc <+> equals) 2 exprDoc
 
 
+-- | Takes care of printing binders with or without uniques.
 instance PprWithOpts (XBinder a) where
   pprWithOpts (XXBinder _) = error "stub"
-  pprWithOpts binder = pprBinderName binder
+  pprWithOpts binder = do
+    opts <- ask
+    if pprOptsDisplayUniques opts then
+      return $ text' $ xBinderName binder <> "_" <> T.pack (show $ xBinderId binder)
+    else
+      return $ text' $ xBinderName binder
 
 
 instance PprWithOpts (XType a) where
-  pprWithOpts (XXType _) = error "stub"
+  pprWithOpts (XXType a) = error "stub"
   pprWithOpts ty = pprType TopPrec ty
   
 data TyPrec = TopPrec | FunPrec | TyOpPrec | TyConPrec
   deriving (Eq, Ord)
 
 pprType :: TyPrec -> XType a -> Reader PprOpts Doc
-pprType _    (XVarTy binder) = pprBinderName binder
+pprType _    (XXType _) = error "stub"
+pprType _    (XVarTy binder) = pprWithOpts binder
 pprType prec t@XFunTy{} = do
   funTyDocs <- mapM (pprType FunPrec) funTys
   return $ parensIf (prec >= FunPrec) $ sep $ punctuate " ->" funTyDocs
@@ -109,7 +118,7 @@ pprType prec (XAppTy f x) = do
   xDoc <- pprType TyConPrec x
   return $ parensIf (prec >= TyConPrec) $ fDoc <+> xDoc
 pprType prec t@XForAllTy{} = do
-  binderDocs <- mapM pprBinderName binders
+  binderDocs <- mapM pprWithOpts binders
   tyDoc <- pprType TopPrec ty
   return $ parensIf (prec >= TyOpPrec) $ "forall" <+> hsep binderDocs <> dot <+> tyDoc
   where
@@ -118,20 +127,66 @@ pprType prec t@XForAllTy{} = do
 
 instance PprWithOpts (XExpr a) where
   pprWithOpts (XXExpr _) = error "stub"
-  pprWithOpts _ = return $ text "tbd"
+  pprWithOpts expr = pprExpr False expr
+
+-- | Expressions can be printed with or without parentheses around them.
+-- @pprExpr@ prints terminal expressions without parens regardless.
+pprExpr :: Bool -> XExpr a -> Reader PprOpts Doc
+pprExpr _ (XXExpr _)           = error "stub"
+pprExpr _ (XVar binder)        = pprWithOpts binder
+pprExpr _ (XVarGlobal extName) = return $ pprExtName extName
+pprExpr _ (XLit lit)           = pprLit lit
+pprExpr _ (XCoercion)          = return "CO"
+pprExpr parens expr = parensIf parens <$> pprExpr' expr
+  where
+    pprExpr' expr@(XApp{}) = do
+      fDoc <- pprExpr True f
+      argDocs <- mapM (pprExpr True) args
+      return $ hang' fDoc 2 (sep argDocs)
+      where
+        (f, args) = collectArguments expr
+
+    pprExpr' expr@(XTyLam{}) = do
+      paramDocs <- mapM pprWithOpts params
+      bodyDoc <- pprWithOpts body
+      return $ hang' ("\\@" <+> sep paramDocs <+> "->") 2 bodyDoc
+      where (params, body) = collectTyBinders expr
+
+    pprExpr' expr@(XLam{}) = do
+      paramDocs <- mapM pprWithOpts params
+      bodyDoc <- pprWithOpts body
+      return $ hang' ("\\" <+> sep paramDocs <+> "->") 2 bodyDoc
+      where (params, body) = collectBinders expr
+
+    pprExpr' (XLet bindings body) = do
+      bindingDocs <- mapM pprWithOpts bindings
+      bodyDoc <- pprWithOpts body
+      return $ "let" <+> (align $ vcat $ bindingDocs) <$$> "in" <+> align bodyDoc
+
+    pprExpr' (XCase match binder alts) = do
+      matchDoc <- pprWithOpts match
+      binderDoc <- pprWithOpts binder
+      altDocs <- mapM pprWithOpts alts
+
+      return $ sep
+        [ sep [ "case" <+> matchDoc, "of" <+> binderDoc <+> "{" ]
+        , indent 2 $ vcat $ altDocs
+        , "}"
+        ]
+
+    pprExpr' (XType ty) = do
+      tyDoc <- pprWithOpts ty
+      return $ "@" <+> tyDoc
 
 instance PprWithOpts (XAlt a) where
   pprWithOpts (XXAlt _) = error "stub"
+  pprWithOpts (XAlt con binders rhs) = do
+    altConDoc <- pprAltCon con
+    binderDocs <- mapM pprWithOpts binders
+    rhsDoc <- pprWithOpts rhs
+    return $ hang' (altConDoc <+> sep binderDocs <+> "->") 2 rhsDoc
 
 -- Terminals
-
--- | Takes care of printing binders with or without uniques.
-pprBinderName binder = do
-  opts <- ask
-  if pprOptsDisplayUniques opts then
-    return $ text' $ xBinderName binder <> "_" <> T.pack (show $ xBinderId binder)
-  else
-    return $ text' $ xBinderName binder
 
 pprBinderMeta :: XBinderMeta -> Reader PprOpts Doc
 pprBinderMeta meta = do
@@ -159,9 +214,31 @@ pprBinderMeta meta = do
     pprOccInfo (OccLoopBreaker isStrong) =
       return $ (if isStrong then "Strong" else "Weak") <+> "Loopbrk"
 
+pprLit :: Lit -> Reader PprOpts Doc
+pprLit = return . pprLit'
+  where
+    pprLit' (MachChar c) = squotes $ pretty c
+    pprLit' (MachStr s) = dquotes $ text $ BS.unpack s
+    pprLit' (MachNullAddr) = "nullAddr#"
+    pprLit' (MachInt i) = pretty i <> "#"
+    pprLit' (MachInt64 i) = pretty i <> "#"
+    pprLit' (MachWord w) = pretty w <> "#"
+    pprLit' (MachWord64 w) = pretty w <> "##"
+    pprLit' (MachFloat f) = "FLOAT" <> parens (pprRational f)
+    pprLit' (MachDouble d) = "DOUBLE" <> parens (pprRational d)
+    pprLit' (MachLabel l) = "LABEL" <> parens (text' l)
+    pprLit' (LitInteger i) = pretty i
+
+    pprRational r = pretty (numerator r) <> "/" <> pretty (denominator r)
+
 pprTyCon (TyCon' t) = text' t
 
--- Helper functions
+pprAltCon (AltDataCon name) = return $ text' name
+pprAltCon (AltLit lit)      = pprLit lit
+pprAltCon (AltDefault)      = return "__DEFAULT"
+
+pprExtName (ExternalName' modName name) = text' modName <> dot <> text' name
+pprExtName ForeignCall' = "<foreign>"
 
 dcolon = text "::"
 
@@ -183,3 +260,18 @@ collectForAlls :: XType a -> ([XBinder a], XType a)
 collectForAlls = go []
   where go acc (XForAllTy binder ty) = go (binder : acc) ty
         go acc ty                    = (reverse acc, ty)
+
+collectArguments :: XExpr a -> (XExpr a, [XExpr a])
+collectArguments = go []
+  where go acc (XApp f x) = go (x : acc) f
+        go acc expr       = (expr, acc)
+
+collectTyBinders :: XExpr a -> ([XBinder a], XExpr a)
+collectTyBinders = go []
+  where go acc (XTyLam p b) = go (p : acc) b
+        go acc expr       = (reverse acc, expr)
+
+collectBinders :: XExpr a -> ([XBinder a], XExpr a)
+collectBinders = go []
+  where go acc (XLam p b) = go (p : acc) b
+        go acc expr       = (reverse acc, expr)
